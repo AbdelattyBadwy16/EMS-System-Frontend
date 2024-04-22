44fb08b8de70ec70507c5106ff00c870902963c9
Abdelatty <abdelattybadwy166@gmail.com>
2024-04-22 22:41:13 +0200

    Solve PageNotFound Problem

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
 import Login from "./pages/Login"
-import RequireAuth from "./helper/RequireAuth"
 import StudentHome from "./pages/StudentHome"
 import AppLayout from "./components/shared/AppLayout"
+import PageNotFound from "./pages/PageNotFound"
 
 function App() {
 
@@ -14,10 +14,11 @@ function App() {
           <Route path="/login" element={<Login></Login>}></Route>
           {/*Private Routes */}
         </Route>
-        <Route element={<RequireAuth></RequireAuth>}>
+        <Route>
           <Route element={<AppLayout></AppLayout>}>
             <Route index element={<Navigate replace to="/login" />} />
-            <Route path='/studenthome' element={<StudentHome></StudentHome>}></Route>            
+            <Route path='/studenthome' element={<StudentHome></StudentHome>}></Route>     
+            <Route path='*' element={<PageNotFound />} />       
           </Route>
         </Route>
       </Routes>
--- src/helper/SaveAuth.tsx
+++ src/helper/SaveAuth.tsx
@@ -2,7 +2,7 @@ import { Navigate, Outlet } from 'react-router-dom';
 import Cookies from 'universal-cookie';
 
 export default function RequireAuth() {
-    const cookie = new Cookies();
+    // const cookie = new Cookies();
     // const token = cookie.get("bearer");
     // check if token found or not
     const token = 1;
