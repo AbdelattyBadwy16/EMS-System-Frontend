592c53ec6070382022a76db1ddeb563cb1be4567
hedra-emad <emadhedra4@gmail.com>
2024-04-23 21:44:37 +0200

    addNavbar

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        futura: "Futura XBlk BT",
+        gesstwo: "GE SS Two",
+        geDinkum:"GE Dinkum",
+      },
+      colors: {
+        navColor: "#1E1E1E",
+        logoutBtnColor: "#D84B4B",
+      },
+    },
+  },
+  plugins: [],
+};
