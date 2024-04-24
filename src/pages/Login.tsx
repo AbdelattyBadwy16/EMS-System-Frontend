7be10329027ae976907e1b272dfbb662edb9b728
Saeed Youseef1 <saeedyouseef228@gmail.com>
2024-04-25 00:36:15 +0200

    Make the login page responsive and some modifications to the css file

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -20,7 +20,7 @@ const Login = () => {
     // Implement your login logic here
     // For now, let's just show an error message if username or password is empty
     if (!username || !password) {
-      setFormData({ ...formData, errorMessage: 'Please enter both username and password' });
+      setFormData({ ...formData, errorMessage: 'Please Enter Both Username And Password' });
     } else {
       // Clear error message if both username and password are provided
       setFormData({ ...formData, errorMessage: '' });
@@ -29,21 +29,23 @@ const Login = () => {
   };
 
   return (
-    <div className='login'>
+    <div>
       <Navbar />
-      <div className="login-container">
-        <div className="south-text">
-          <h1>South Vally Universty</h1>
+      <div className='login'>
+        <div className="login-container">
+          <div className="south-text">
+            <h1>User Login</h1>
+          </div>
+          <form>
+            <input type="text" name="username" value={username} onChange={handleInputChange} placeholder="Username" /><br /><br />
+            <input type="password" name="password" value={password} onChange={handleInputChange} placeholder="Password" /><br /><br />
+            <button className='login-btn' type="button" onClick={loginfun}>Login</button>
+          </form>
+          { <div className="error-message" >{errorMessage}</div>}
         </div>
-        <form>
-          <input type="text" name="username" value={username} onChange={handleInputChange} placeholder="Username" /><br /><br />
-          <input type="password" name="password" value={password} onChange={handleInputChange} placeholder="Password" /><br /><br />
-          <button className='login-btn' type="button" onClick={loginfun}>Login</button>
-        </form>
-        {errorMessage && <div className="error-message">{errorMessage}</div>}
       </div>
     </div>
-  );
+    );
 };
 
 export default Login;
