34b8e43518e8980f77002153756295ec4f6ba6b0
Saeed Youseef1 <saeedyouseef228@gmail.com>
2024-05-06 20:20:52 +0300

    8.20

--- src/App.tsx
+++ src/App.tsx
@@ -25,6 +25,7 @@ function App() {
           {/*Private Routes */}
           <Route element={<PersistLogin></PersistLogin>}>
             <Route element={<RequireAuth></RequireAuth>}>
+              
               <Route element={<AppLayout></AppLayout>}>
                 <Route path='/studenthome' element={<StudentHome></StudentHome>}></Route>
                 <Route path='/facultyhome' element={<FacultyHome></FacultyHome>}></Route>
